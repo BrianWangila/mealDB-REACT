5f9a98700c2abe56334a370c5b70a3fe642579bc
Brian Wangila <bwanjalah@gmail.com>
2022-07-21 13:45:38 +0300

    add mealcard

--- src/components/MealCard/MealCard.js
+++ src/components/MealCard/MealCard.js
@@ -5,11 +5,11 @@ export default function MealCard({mealName, mealThumbnail, mealId}){
   return(
     
     <div className="card" style={{width: 18+"rem"}}>
-      <img src={mealThumbnail} class="card-img-top" alt="..."/>
+      <img src={mealThumbnail} class="card-img-top" alt={mealName}/>
       <div className="card-body">
         <h5 className="card-title">{mealName}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <Link to={`/meal/${mealId}`} class="btn btn-primary">Go somewhere</Link>
+        <Link to={`/meal/${mealId}`} class="btn btn-primary">Details</Link>
       </div>
     </div>
    
